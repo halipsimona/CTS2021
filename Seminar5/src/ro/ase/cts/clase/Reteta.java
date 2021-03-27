eed36051bfd31743c5725413990e45af633b179f
halipsimona <halip.simona@yahoo.com>
2021-03-27 12:10:51 +0200

    Seminar 5 Prototype

---
+++ Seminar5/src/ro/ase/cts/clase/Reteta.java
@@ -0,0 +1,55 @@
+package ro.ase.cts.clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reteta implements AbstractPrototype{
+
+	private List<String> denumiriSolutii;
+	private List<Integer> cantitati;
+	public List<String> getDenumiriSolutii() {
+		return denumiriSolutii;
+	}
+	public void setDenumiriSolutii(List<String> denumiriSolutii) {
+		this.denumiriSolutii = denumiriSolutii;
+	}
+	public List<Integer> getCantitati() {
+		return cantitati;
+	}
+	public void setCantitati(List<Integer> cantitati) {
+		this.cantitati = cantitati;
+	}
+	public Reteta(List<String> denumiriSolutii, List<Integer> cantitati) {
+		super();
+		//TODO: validari
+		this.denumiriSolutii = denumiriSolutii;
+		this.cantitati = cantitati;
+	}
+	public Reteta() {
+		//nu facem validari
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Reteta [denumiriSolutii=");
+		builder.append(denumiriSolutii);
+		builder.append(", cantitati=");
+		builder.append(cantitati);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+
+	@Override
+	public AbstractPrototype copiaza() {
+		// TODO Auto-generated method stub
+		Reteta copie=new Reteta();
+		copie.cantitati=new ArrayList<>();
+		copie.cantitati.addAll(this.cantitati);
+		copie.denumiriSolutii=new ArrayList<>();
+		copie.denumiriSolutii.addAll(this.denumiriSolutii);
+		return copie;
+	}
+	
+	
+}
---
+++ Seminar5/src/ro/ase/cts/program/Program.java
@@ -0,0 +1,24 @@
+package ro.ase.cts.program;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ro.ase.cts.clase.Reteta;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> listaDenumiriSolutii=new ArrayList<>();
+		listaDenumiriSolutii.add("apa");
+		listaDenumiriSolutii.add("etanol");
+		List<Integer> listaCantitati=new ArrayList<>();
+		listaCantitati.add(20);
+		listaCantitati.add(200);
+		Reteta reteta1=new Reteta(listaDenumiriSolutii,listaCantitati);
+		Reteta reteta2=(Reteta) reteta1.copiaza();
+		System.out.print(reteta1);
+		System.out.print(reteta2);
+	}
+
+}
